c0175b5500172aae8aac6f57d49346ea16fa344f
SED4906 <sed4906birdie@gmail.com>
2022-12-11 10:03:32 -0800

    initial stuff

---
+++ build.rs
@@ -0,0 +1,19 @@
+use std::{env, error::Error};
+
+fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    // Get the name of the package.
+    let kernel_name = env::var("CARGO_PKG_NAME")?;
+
+    // Tell rustc to pass the linker script to the linker.
+    println!("cargo:rustc-link-arg-bin={kernel_name}=--script=.cargo/linker.ld");
+
+    // Have cargo rerun this script if the linker script or CARGO_PKG_ENV changes.
+    println!("cargo:rerun-if-changed=.cargo/linker.ld");
+    println!("cargo:rerun-if-env-changed=CARGO_PKG_NAME");
+
+    cc::Build::new()
+    .file("src/switch.S")
+    .compile("switch.o");
+
+    Ok(())
+}
