f9c9ff540cc3c79527506880613e5dc820ccf330
SED <sed4906birdie@gmail.com>
2023-01-16 05:36:53 -0800

    change 9

--- build.rs
+++ build.rs
@@ -15,7 +15,6 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     .compiler("clang")
     .file("src/stubs.S")
     .file("src/switch.S")
-    .file("src/vmm.S")
     .compile("asm.o");
 
     Ok(())
